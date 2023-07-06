60041948efb0e6208ef560ad4a3eb89d2164b1de
cesarios <cesarios11@gmail.com>
2023-07-06 17:50:36 -0500

    Se crea un proyecto para ser publicado en un contenedor en Docker. Se agrega un documento word con los pasos a seguir para realizar la publicacion localmente.

---
+++ DockerApp/DockerDotNetApp/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerDotNetApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    //TODO: No importa el DNS, solo importa el puerto aleatorio que se eligió (5024).
+                    //Para evitar que docker tenga problemas con el puerto
+                    webBuilder.UseUrls("http://*:5024");
+                });
+    }
+}
